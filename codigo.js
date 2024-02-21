102c17d28bf6b80bbe87c5ab815d337e5d8ef976
desqui92 <43731462+desqui92@users.noreply.github.com>
2024-02-21 20:43:38 -0300

    Reemplazo

--- codigo.js
+++ codigo.js
@@ -1,15 +1,22 @@
+if (window.location.href.indexOf("watch") > -1) {
+var style = (function() {
+    // Create the <style> tag
+    var style = document.createElement("style");
+
+    // WebKit hack
+    style.appendChild(document.createTextNode(""));
+
+    // Add the <style> element to the page
+    document.head.appendChild(style);
+    return style;
+})();
+
 var axelin = setInterval(function () {
-if(document.getElementsByClassName("ytp-ad-skip-button-modern")[0] !== null){
-if(document.getElementsByClassName("ytp-ad-skip-button-modern")[0] !== undefined){
-    document.getElementsByClassName("ytp-ad-skip-button-modern")[0].click();
+if(document.body.getElementsByClassName("ytp-ad-skip-button-modern")[0] !== undefined){
+if(document.body.getElementsByClassName("ytp-ad-skip-button-modern")[0] !== null){
+    document.body.getElementsByClassName("ytp-ad-skip-button-modern")[0].click();
  }
 }
-if(document.getElementsByClassName("ytp-ad-preview-container-detached")[0] !== undefined){
-let div = document.createElement('div');
-div.classList.add('test');
-let text = document.createTextNode('Test');
-div.appendChild(text);
-document.getElementsByClassName("ytp-ad-preview-container-detached")[0].appendChild(div);
-document.getElementsByClassName("test")[0].innerHTML = `<span class="ytp-ad-skip-button-container ytp-ad-skip-button-container-detached" style="opacity: 0.5;"><button class="ytp-ad-skip-button-modern ytp-button"><div class="ytp-ad-text ytp-ad-skip-button-text-centered ytp-ad-skip-button-text" id="ad-text:6" style="">Omitir</div><span class="ytp-ad-skip-button-icon-modern"><svg height="100%" viewBox="-6 -6 36 36" width="100%"><path d="M5,18l10-6L5,6V18L5,18z M19,6h-2v12h2V6z" fill="#fff"></path></svg></span></button></span>`;
-}
 }, 100);
+}
+
